3f13b5cabb86ba727aeb7340dd6f32dfb2a7cb54
pearlsoflutra5 <pearlsoflutra5@gmail.com>
2019-02-15 06:58:20 -0700

    added all chapter 11 files

---
+++ Chapter 11/Exercise 11.13/Exercise11point13.java
@@ -0,0 +1,37 @@
+/*
+Kacie Rae
+2-13-19
+Exercise 11.13
+*/
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Exercise11point13 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		System.out.print("Enter 10 integers: ");
+		for (int i = 0; i < 10; i++){
+			list.add(input.nextInt());
+		}
+		
+		removeDuplicate(list);
+		
+		System.out.print("The distinct integers are: ");
+		for (int i = 0; i < list.size(); i++){
+			System.out.print(list.get(i) + " ");
+		}
+	}
+	
+	public static void removeDuplicate(ArrayList<Integer> list) {
+		for (int i = 0; i < list.size(); i++){
+			for (int j = i + 1; j < list.size(); j++){
+				if (list.get(i) == list.get(j)){
+					list.remove(j);
+				}
+			}
+		}
+	}
+}
